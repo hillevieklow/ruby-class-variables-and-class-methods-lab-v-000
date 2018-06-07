61df35b817c58e398fd3927dd634a9247531990c
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-07 01:40:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,18 @@
-require 'pry'
 class Song
+
   attr_accessor :name, :artist, :genre
+
   @@count = 0
   @@artists = []
   @@genres = []
 
   def initialize(name, artist, genre)
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genres
   end
 
   def self.count
@@ -41,12 +42,13 @@ class Song
   def self.artist_count
     artist_hash = {}
     @@artists.each do |artist|
-      if artist_hash.include?(artist)
+      if genre_hash.include?(artist)
         artist_hash[artist] += 1
       else
         artist_hash[artist] = 1
       end
     end
     artist_hash
-  end
+  endle
+
 end
